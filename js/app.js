f3160dba325e8f36bbec6aae8430ff03437ce566
Karen Borrero <keba2503@gmail.com>
2021-01-24 19:30:33 +0000

    functional suma por item y por categoria.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-//Selectors
+//Selectors Generacion
 const formularioGeneracion = document.querySelector('#generacion');
-const formularioConsumo = document.querySelector('#consumo');
 const listado = document.querySelector('#listado-generacion tbody')
-const listadoConsumo = document.querySelector('#listado-consumo tbody');
 const botonEliminar = document.querySelector('#boton-eliminar ul')
+
+
+
+//Selectores consumo
+const formularioConsumo = document.querySelector('#consumo');
+const listadoConsumo = document.querySelector('#listado-consumo tbody');
 const botonEliminarConsumo = document.querySelector('#boton-eliminar-consumo ul')
-const total = document.querySelector('#suma-dispositivo tbody');
-const totalConsumo = document.querySelector('#suma-dispositivo-consumo tbody');
 
 //Eventos
 
@@ -17,46 +19,42 @@ function eventListeners() {
     formularioConsumo.addEventListener('submit', agregarConsumo);
 }
 
-
 class Generacion {
     constructor() {
         this.dispositivos = [];
-        this.suma = [];
-        this.sumaConsumo = [];
-              }
-
-
-
-
-
+        this.dispositivosConsumo = [];
+        this.sumaGeneracion = 0;
+        this.sumaConsumo = 0;
+    }
 
     nuevoDispositivo(dispositivo) {
         this.dispositivos = [...this.dispositivos, dispositivo];
-        this.totalGeneracion();
-        this.totalConsumo();
-     
-    }
 
-
-    totalGeneracion() {
-        const totaling = this.dispositivos;
-        const totaling2 = (totaling[0]);
-        this.suma = (totaling2.cantidad * totaling2.potencia / 12) * totaling2.horas;  
-        
+        this.calcularRestanteGeneracion();
       
+
     }
 
-    totalConsumo() {
-        const totaling = this.dispositivos;
-        const totaling2 = (totaling[0]);
-        this.sumaConsumo = (totaling2.cantidadConsumo * totaling2.potenciaConsumo / 12) * totaling2.horasConsumo;      
-         
-       
+    nuevoDispositivoConsumo(dispositivoConsumo) {
+        this.dispositivosConsumo = [...this.dispositivosConsumo, dispositivoConsumo];
+        this.calcularRestanteConsumo();
     }
 
-    
-    
+    calcularRestanteGeneracion() {
 
+        let total = 0;
+        this.dispositivos.forEach(function (dispositivo) { total += dispositivo.totalPrueba; });
+        this.sumaGeneracion = total;
+        console.log(this.sumaGeneracion)
+    }
+
+    calcularRestanteConsumo() {
+
+        let total = 0;
+        this.dispositivosConsumo.forEach(function (dispositivoConsumo) { total += dispositivoConsumo.totalPruebaConsumo; });
+        this.sumaConsumo = total;
+        console.log(this.sumaConsumo)
+    }
 }
 
 
@@ -96,7 +94,7 @@ class UI {
         //iterar sobre los gastos
 
         dispositivos.forEach(dispositivo => {
-            const { nombre, cantidad} = dispositivo;
+            const { nombre, cantidad, totalPrueba } = dispositivo;
 
             const row = document.createElement('tr');
 
@@ -112,6 +110,7 @@ class UI {
             row.innerHTML =
                 `<td>${cantidad}</td>
         <td>${nombre}</td>
+        <td>${totalPrueba}</td>
                          `;
 
 
@@ -121,35 +120,19 @@ class UI {
 
         });
         
-    }
-
- 
-
-    actualizargeneracion(suma) {
-
-        
-
-        const sumado = suma.toFixed(2);
-        const totales = document.createElement('tr')
-        totales.innerHTML =  `<td>${sumado}</td> `;
-        total.appendChild(totales)
-
-    }
-    
-
-    
+    }    
 }
 
 
 class UICONSUMO {
 
-    agregarConsumoListado(dispositivos) {
+    agregarConsumoListado(dispositivosConsumo) {
         // this.limpiarHTML();
     
         //iterar sobre los gastos
     
-        dispositivos.forEach(dispositivo => {
-            const { nombreConsumo, cantidadConsumo} = dispositivo;
+        dispositivosConsumo.forEach(dispositivoConsumo => {
+            const { nombreConsumo, cantidadConsumo, totalPruebaConsumo} = dispositivoConsumo;
     
             const row = document.createElement('tr');
     
@@ -165,6 +148,7 @@ class UICONSUMO {
             row.innerHTML =
                 `<td>${cantidadConsumo}</td>
         <td>${nombreConsumo}</td>
+        <td>${totalPruebaConsumo}</td>
                          `;
     
     
@@ -175,82 +159,52 @@ class UICONSUMO {
         });
         
     }
-
-    actualizargeneracion(sumaConsumo) {
-
-        const sumado = sumaConsumo.toFixed(2);
-        const totales = document.createElement('tr')
-        totales.innerHTML =  `<td>${sumado}</td> `;
-        totalConsumo.appendChild(totales)
-
-    }
+   
 }
 
 const ui = new UI();
-let generacion;
-let suma;
 const uiConsumo = new UICONSUMO();
-
-
-
-
-
-
+generacion = new Generacion();
+consumo = new Generacion();
 
 
 
 //Funciones
 
 function agregarGeneracion(e) {
-
-    generacion = new Generacion();
-
     e.preventDefault();
-
-
     //leer datos para validacion de formulario
     const nombre = document.querySelector('#nombre').value;
     const cantidad = Number(document.querySelector('#cantidad').value);
     const potencia = Number(document.querySelector('#potencia').value);
     const horas = Number(document.querySelector('#horas').value);
-
+    const consumicion = (cantidad * potencia / 12) * horas;
+    const totalPrueba = Number(consumicion.toFixed(2));
 
     // Comprobar que los campos no esten vacios
     if (nombre === '' || cantidad === '' || potencia === '' || horas === '') {
         // 2 parametros: mensaje y tipo
         ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
 
-       
-
         return;
 
-    }else {
+    } else {
 
-    //Generar objeto con la generacion
+        //Generar objeto con la generacion
+        const dispositivo = { nombre, cantidad, potencia, horas, totalPrueba, id: Date.now() };
 
-    const dispositivo = { nombre, cantidad, potencia, horas, id: Date.now() };
+        //añade nueva generacion
+        generacion.nuevoDispositivo(dispositivo);
 
-    //añade nueva generacion
-    generacion.nuevoDispositivo(dispositivo);
+        // generacion.nuevoDispositivo(dispositivo);
+        ui.imprimirAlerta('Generacion agregada correctamente');
 
+        //Imprimir lista de generacion
+        const { dispositivos } = generacion;
+        console.log(dispositivos);
+        ui.agregarGeneracionListado(dispositivos);
 
-    // generacion.nuevoDispositivo(dispositivo);
-    ui.imprimirAlerta('Generacion agregada correctamente');
-
-    //Imprimir lista de generacion
-    const { dispositivos, suma } = generacion;
-    ui.agregarGeneracionListado(dispositivos);
-
-
-
-    //Imprimir Totales de cada dispositivo
-    generacion.totalGeneracion()
-    ui.actualizargeneracion(suma);
-
- 
-
-    }
-
+   }
 
 
 }
@@ -262,53 +216,40 @@ function agregarGeneracion(e) {
 
 
 function agregarConsumo(e) {
-    e.preventDefault();
+   e.preventDefault();
 
-    consumo = new Generacion();
-
+//     //leer datos para validacion de formulario
     const nombreConsumo = document.querySelector('#nombre-consumo').value;
     const cantidadConsumo = Number(document.querySelector('#cantidad-consumo').value);
     const potenciaConsumo = Number(document.querySelector('#potencia-consumo').value);
     const horasConsumo = Number(document.querySelector('#horas-consumo').value);
+    const consumicionConsumo = (cantidadConsumo * potenciaConsumo / 12) * horasConsumo;
+    const totalPruebaConsumo = Number(consumicionConsumo.toFixed(2));
 
-
-  
-
+    // Comprobar que los campos no esten vacios
     if (nombreConsumo === '' || cantidadConsumo === '' || potenciaConsumo === '' || horasConsumo === '') {
         // 2 parametros: mensaje y tipo
         ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
+
         return;
 
-    }else { 
+    } else {
 
-          //Generar objeto con la generacion
+        //Generar objeto con la generacion
+        const dispositivoConsumo = { nombreConsumo, cantidadConsumo, potenciaConsumo, horasConsumo, totalPruebaConsumo, id: Date.now() };
 
-    const dispositivoConsumo = { nombreConsumo, cantidadConsumo, potenciaConsumo, horasConsumo, idConsumo: Date.now() };
-       
+        //añade nueva generacion
+        consumo.nuevoDispositivoConsumo(dispositivoConsumo);
 
-   consumo.nuevoDispositivo(dispositivoConsumo);
+        // generacion.nuevoDispositivo(dispositivo);
+        ui.imprimirAlerta('Generacion agregada correctamente');
 
-   // generacion.nuevoDispositivo(dispositivo);
-   ui.imprimirAlerta('Consumo agregado correctamente');
+        //Imprimir lista de generacion
+        const { dispositivosConsumo } = consumo;
+        console.log(dispositivosConsumo);
+        uiConsumo.agregarConsumoListado(dispositivosConsumo);
 
-
-   //Imprimir lista de generacion
- 
-   const { dispositivos , sumaConsumo} = consumo;
-
-
-
-   uiConsumo.agregarConsumoListado(dispositivos);
-
-
-
-
-       //Imprimir Totales de cada dispositivo
-      consumo.totalConsumo()
-       uiConsumo.actualizargeneracion(sumaConsumo);
-
-
-
+   }
 
 
     }
@@ -317,13 +258,9 @@ function agregarConsumo(e) {
 
 
 
-///Pruebas
 
 
 
 
 
-
-    
-}
 
